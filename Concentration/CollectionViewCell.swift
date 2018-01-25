7770fa548fff7128df74fed5ee0fec37563150ee
Roman Shveda <shvedaroman95@gmail.com>
2018-01-25 17:17:24 +0200

    Initial Commit

---
+++ Concentration/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  Concentration
+//
+//  Created by Roman Shveda on 1/11/18.
+//  Copyright © 2018 Roman Shveda. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell{
+    
+    @IBOutlet weak var cardLabel: UILabel!
+    var id = 0
+    
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,29 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Roman Shveda on 1/4/18.
+//  Copyright © 2018 Roman Shveda. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+    init(numberOfPairsCards: Int){
+        var unShuffled = [Card]()
+        for _ in 1...numberOfPairsCards {
+            let card = Card()
+            unShuffled+=[card,card]
+        }
+        //Shuffling array (need improvment)
+        for _ in 0..<unShuffled.count{
+            let rand = (unShuffled.count - 1).arc4random
+            cards.append(unShuffled.remove(at: rand))
+        }
+    }
+}
+
+
